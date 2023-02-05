a5cc2b6ee07cafb17d35356422fef457855ecbfd
key-door <101494707+key-door@users.noreply.github.com>
2023-02-05 12:36:11 +0900

    fin

--- cpp-module00/ex00/megaphone.cpp
+++ cpp-module00/ex00/megaphone.cpp
@@ -6,9 +6,35 @@
 /*   By: keys <keys@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 21:43:45 by keys              #+#    #+#             */
-/*   Updated: 2023/01/12 21:44:01 by keys             ###   ########.fr       */
+/*   Updated: 2023/02/05 12:29:53 by keys             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	c;
+	int		k;
+
+	i = 1;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	while (i < argc)
+	{
+		k = 0;
+		while (argv[i][k])
+		{
+			c = toupper(argv[i][k]);
+			std::cout << c;
+			k++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
