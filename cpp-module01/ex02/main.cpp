a748b1a5b79958265f4f58b00095d0a080c79189
key-door <101494707+key-door@users.noreply.github.com>
2023-08-20 18:26:47 +0900

    fix

--- cpp-module01/ex02/main.cpp
+++ cpp-module01/ex02/main.cpp
@@ -1,14 +1,19 @@
-#include "Zombie.hpp"
-# define N 10
+#include <iostream>
+#include <string>
 
 int main()
 {
-	Zombie *z = zombieHorde(N, "Foo");
-	
-	for(int i = 0; i < N; i++){
-		std::cout << "index :" << i <<std::endl;
-		z[i].announce();
-	}
-	delete[] z;
+		std::string str("HI THIS IS BRAIN");
+		std::string *ptr = &str;
+		std::string& ref = str;
+
+		std::cout << "str addr: " << &str << std::endl;
+		std::cout << "ptr addr: " << ptr << std::endl;
+		std::cout << "ref sddr: " << &ref << std::endl;
+
+		std::cout << "str variable: " << str << std::endl;
+		std::cout << "ptr variable: " << *ptr << std::endl;
+		std::cout << "ref variable: " << ref << std::endl;
+		
 	return 0;
 }
