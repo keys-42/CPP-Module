24bba62540c8abd92ebb782eda353ddc575efc41
key-door <101494707+key-door@users.noreply.github.com>
2023-08-20 18:54:10 +0900

    add Weapon

--- cpp-module01/ex03/Weapon.cpp
+++ cpp-module01/ex03/Weapon.cpp
@@ -1,1 +1,16 @@
 #include "Weapon.hpp"
+
+Weapon::Weapon(){};
+Weapon::~Weapon(){};
+
+const std::string& Weapon::get_type() const
+{
+	return type_;
+}
+
+void Weapon::set_type(const std::string& new_type)
+{
+	type_ = new_type;
+}
+
+
--- cpp-module01/ex03/Weapon.hpp
+++ cpp-module01/ex03/Weapon.hpp
@@ -1,4 +1,16 @@
-#ifndef WEPON_HPP
+#ifndef WEAPON_HPP
 # define WEAPON_HPP
+#include <string>
+
+class Weapon{
+    public:
+        Weapon();
+        ~Weapon();
+        const std::string& get_type() const ;
+        void  set_type(const std::string& new_type);
+
+    private:
+            std::string type_;
+};
 
 #endif
