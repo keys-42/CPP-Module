4fb4aecae5d19d7d0473312daeeb954f72fc90f6
libknt <101494707+libknt@users.noreply.github.com>
2023-08-26 14:50:34 +0900

    fix bug ctrl + d

--- cpp-module01/ex05/main.cpp
+++ cpp-module01/ex05/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <limits>
 #include "Harl.hpp"
+#include <sstream>
 
 int main()
 {
@@ -8,7 +10,19 @@ int main()
 
 	do{
 		std::cout << "level: ";
-		std::cin >> s;
+		if(!std::getline(std::cin, s))
+		{
+			if(std::cin.eof()){
+				std::cin.clear();
+				break;
+			}
+			else {
+				std::cin.clear();
+				if(std::cin.rdbuf()->in_avail())
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				s.clear();
+			}
+		}
 		h.complain(s);
 	} while (s.compare("exit"));
 
