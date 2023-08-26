1c0de2429e784c52394209bcce53db430235adb3
libknt <101494707+key-door@users.noreply.github.com>
2023-08-26 21:52:15 +0900

    change

--- cpp-module01/ex01/main.cpp
+++ cpp-module01/ex01/main.cpp
@@ -6,7 +6,7 @@ int main()
 	Zombie *z = zombieHorde(N, "Foo");
 	
 	for(int i = 0; i < N; i++){
-		std::cout << "index :" << i <<std::endl;
+		std::cout << "index : " << i << ": ";
 		z[i].announce();
 	}
 	delete[] z;
