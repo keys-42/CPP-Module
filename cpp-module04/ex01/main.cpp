811e9f5c8ec867d6a8b4530dac59bace44b348d8
libknt <101494707+key-door@users.noreply.github.com>
2023-09-03 20:00:38 +0900

    change

--- cpp-module04/ex01/main.cpp
+++ cpp-module04/ex01/main.cpp
@@ -3,17 +3,24 @@
 #include "Cat.hpp"
 #include "WrongCat.hpp"
 
-
 int main()
 {
+	std::cout << "==========================" << std::endl;
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	std::cout << "==========================" << std::endl;
 	delete j;
 	delete i;
+	std::cout << "==========================" << std::endl;
 
-	const Animal* d= new Dog();
-
-	delete d;
+	Animal* d = new Dog[2];
+	std::cout << "==========================" << std::endl;
+	delete[] d;
+	std::cout << "==========================" << std::endl;
+	Animal* c = new Cat[2];
+	std::cout << "==========================" << std::endl;
+	delete[] c;
+	std::cout << "==========================" << std::endl;
 	
 	return 0;
 }
