11ab638683dd2aeabe4d6f5766fb5239fcc69ba4
libknt <101494707+key-door@users.noreply.github.com>
2023-09-19 14:05:52 +0900

    add

---
+++ cpp-module09/vector/PmergeMe.cpp
@@ -0,0 +1,315 @@
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {};
+
+PmergeMe::PmergeMe(const PmergeMe & r): mainChain_(r.mainChain_) {};
+
+PmergeMe::~PmergeMe() {};
+
+PmergeMe & PmergeMe::operator= (const PmergeMe &other) {
+    if( this != &other ) {
+        this->mainChain_ = other.mainChain_;
+    }
+    return *this;
+}
+
+
+typedef std::list<int>::const_iterator ConstIntListIter;
+typedef std::list<int>::iterator IntListIter;
+
+typedef std::list<std::list<int>::const_iterator> ConstIteratorList;
+typedef std::list<std::list<int>::const_iterator>::const_iterator ConstIteratorListIterator;
+
+
+
+
+
+
+
+/**
+ * pair comparison
+*/
+bool     PmergeMe::pairCompare( std::list<int> & lst, int pairSize, int start ) {
+    if( getElementAtIndex(lst, start) < getElementAtIndex(lst,start + pairSize) )
+        return 1;
+    return 0;
+}
+
+void    PmergeMe::pairSwap( std::list<int> & lst, int pairSize, int start) {
+    std::list<int>::iterator left = advanceTo(lst, start);
+    std::list<int>::iterator left_end = advanceTo(lst, start + pairSize);
+
+    std::list<int>::iterator right = left_end;
+    std::list<int>::iterator right_end = advanceTo(lst, start + 2 * pairSize);
+
+    std::list<int> tmp;
+    tmp.splice(tmp.begin(), lst, right, right_end);
+
+    lst.splice(right, lst, left, left_end);
+    lst.splice(left, tmp, tmp.begin(), tmp.end());
+}
+
+bool PmergeMe::hasPair(std::list<int> & lst, int pairSize, size_t start) {
+    if(advanceTo(lst,start + pairSize + pairSize - 1) == lst.end())
+        return false;
+    return true;
+}
+
+void    PmergeMe::makePair(std::list<int> & lst, std::list<int> & subChain, int pairSize) {
+
+    for ( size_t i=0; i < lst.size(); ) {
+
+        if( ! hasPair(lst, pairSize,i) )
+        {
+            if ( advanceTo(lst,i) != lst.end())
+                subChain.splice(subChain.begin(), lst, advanceTo(lst, i), lst.end() );
+            break;
+        }
+        if(pairCompare(lst, pairSize, i)) {
+            pairSwap(lst, pairSize, i);
+        }
+
+        i+=pairSize + pairSize;
+    }
+}
+
+
+
+
+/**
+ * separate mainChain SubChain
+*/
+
+void PmergeMe::separateMainChainAndSubChain(std::list<int> & mainChain, std::list<int> & subChain, int pairSize) {
+    std::list<int>::iterator left;
+    std::list<int>::iterator left_end;
+
+    for ( int i=1; ; ++i ) {
+        left = advanceTo(mainChain,  i*pairSize );
+        if( left == mainChain.end() )
+            break;
+        left_end = advanceTo(mainChain, i*pairSize + pairSize);
+        subChain.splice(advanceTo(subChain, (i-1) * pairSize), mainChain, left, left_end);
+    } 
+}
+
+
+
+
+/**
+ * insert first subPair
+*/
+
+void PmergeMe::insertAtTheStart(std::list<int> & mainChain, std::list<int> & subChain, int pairSize) {
+    mainChain.insert(mainChain.begin(), subChain.begin(),  advanceTo(subChain, pairSize) );
+}
+
+
+
+/**
+ * insert
+*/
+int PmergeMe::jacobsthalNumber(int n) {
+
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+
+    return jacobsthalNumber(n - 1) + (2 * (jacobsthalNumber(n - 2)));
+}
+
+bool PmergeMe::isKey(std::list<int> & lst, int index, int key) {
+    if (getElementAtIndex(lst, index) >= key) return true;
+    else return false;
+}
+
+int PmergeMe::lower_bound(std::list<int> & lst, int key, int right) {
+    int left = -1;
+    while (right - left > 1) {
+        int mid = left + (right - left) / 2;
+
+        if (isKey(lst, mid, key)) right = mid;
+        else left = mid;
+    }
+
+    return right;
+}
+
+int  PmergeMe::binarySearch(std::list<int> & mainChain, int key, int pairSize, UnpairedData& unpair, int endpoint) { 
+    std::list<int> lst;
+    if(unpair.position == -1) {
+        (void)unpair;
+        for(ConstIntListIter it=mainChain.begin(); it != mainChain.end(); it = advanceIt(mainChain, it, pairSize)) {
+            lst.push_back(*it);
+        }
+    } else {
+        int index = 0;
+        for(ConstIntListIter it=mainChain.begin(); it != mainChain.end(); ){
+            if(unpair.position == index) {
+                lst.push_back(*it);
+                std::advance(it, unpair.length);
+                ++index;
+                continue;
+            }
+            lst.push_back(*it);
+            std::advance(it, pairSize);
+            ++index;
+        }
+    }
+    return lower_bound(lst, key, endpoint);
+}
+
+void PmergeMe::insertSubChain(std::list<int> & mainChain,int  insertPosition, ConstIntListIter begin, ConstIntListIter end) {
+    mainChain.insert(advanceTo(mainChain, insertPosition), begin, end);
+}
+
+void PmergeMe::insertionFromSubIntoMain(std::list<int> & mainChain, std::list<int> & subChain, int pairSize, UnpairedData& unpair) {
+
+    
+    #ifdef SORT
+        this->printDebug<std::list<int> >(mainChain, subChain, pairSize, "Before sort");
+    #endif
+
+    int maxSize = subChain.size()/pairSize;
+    int itr_index =0;
+    int insertSize=0;
+    for ( int n=3; ; ++n) {
+        for( int i=jacobsthalNumber(n); i > jacobsthalNumber(n-1); --i) {
+            if( i > maxSize ) {
+                i = maxSize;
+                if( i == jacobsthalNumber(n-1)) break;
+                itr_index = binarySearch(mainChain, getElementAtIndex(subChain, (i-1)*pairSize), pairSize, unpair, insertSize + i - 1);
+                insertSubChain(mainChain, itr_index, advanceTo(subChain, (i-1)*pairSize), subChain.end());
+                unpair.position = itr_index;
+                ++insertSize;
+                continue;
+            }
+            if(unpair.position != -1) {
+                if(unpair.value >= getElementAtIndex(subChain, (i-1)*pairSize))
+                    ++unpair.position;
+            }
+            itr_index = binarySearch(mainChain, getElementAtIndex(subChain, (i-1)*pairSize), pairSize, unpair, insertSize + i - 1);
+            insertSubChain(mainChain, pairSize*itr_index, advanceTo(subChain, (i-1)*pairSize), advanceTo(subChain, (i-1)*pairSize + pairSize));
+            ++insertSize;
+        }
+        if(jacobsthalNumber(n) > maxSize) break;
+    }
+    subChain.clear();
+    #ifdef SORT
+        this->printDebug<std::list<int> >(mainChain, subChain, pairSize, "After sort");
+    #endif
+}
+
+void PmergeMe::mergeInsertionSort(std::list<int> & mainChain,int pairSize) {
+    if (mainChain.size() <= 2 * static_cast<size_t>(pairSize)) {
+        #ifdef SEPARATE
+            this->printDebug<std::list<int> >( mainChain, std::list<int>(), pairSize, "Before pair");
+        #endif
+        #ifdef PAIR
+            this->printDebug<std::list<int> >( mainChain, std::list<int>(), pairSize, "Before pair");
+        #endif
+        if(mainChain.size()  <= static_cast<size_t>(pairSize)) return;
+        if(!pairCompare(mainChain, pairSize, 0)) {
+            pairSwap(mainChain, pairSize, 0);
+        }
+        #ifdef PAIR
+            this->printDebug<std::list<int> >( mainChain, std::list<int>(), pairSize, "After pair");
+        #endif
+        #ifdef SEPARATE
+            this->printDebug<std::list<int> >( mainChain, std::list<int>(), pairSize, "After pair");
+        #endif
+        return ; 
+    } 
+    
+    std::list<int> subChain;
+    #ifdef PAIR
+        this->printDebug<std::list<int> >( mainChain, subChain, pairSize, "Before pair");
+    #endif
+    makePair( mainChain, subChain, pairSize);
+    #ifdef PAIR
+        this->printDebug<std::list<int> >( mainChain, subChain, pairSize, "After pair");
+    #endif
+    UnpairedData unpair;
+    unpair.length = subChain.size();
+    unpair.value = *(subChain.begin());
+    unpair.position = -1;
+    
+    mergeInsertionSort(mainChain, pairSize * 2);
+
+    #ifdef SEPARATE
+        this->printDebug<std::list<int> >(mainChain, subChain, pairSize, "Before separating.");
+    #endif
+    separateMainChainAndSubChain(mainChain, subChain, pairSize);
+    #ifdef SEPARATE
+        this->printDebug<std::list<int> >(mainChain, subChain, pairSize, "After separating.");
+    #endif
+
+    #ifdef INSERTATTHESTART
+        this->printDebug<std::list<int> >(mainChain, subChain, pairSize, "Before Insert the first value.");
+    #endif
+    insertAtTheStart(mainChain, subChain, pairSize);
+    #ifdef INSERTATTHESTART
+        this->printDebug<std::list<int> >(mainChain, subChain, pairSize, "After Insert the first value.");
+    #endif
+
+    insertionFromSubIntoMain(mainChain, subChain, pairSize, unpair);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void    PmergeMe::initContainer(int size, int numbers[]) {
+    for (int i = 0; i < size; ++i) { mainChain_.push_back(numbers[i]);}
+}
+
+void PmergeMe::FordJohnsonAlgorithm(int size, int numbers[]) {
+    this->initContainer(size, numbers);
+    std::list<int> arr(mainChain_);
+    try {
+        mergeInsertionSort(this->mainChain_, 1);
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
+
+    std::cout << "sort 前" << std::endl;
+    PmergeMe::print<std::list<int>, ConstIntListIter>(this->mainChain_);
+    mainChain_.sort();
+    PmergeMe::print<std::list<int>, ConstIntListIter>(this->mainChain_);
+    std::cout << "sort後" << std::endl;
+    if (isSorted(mainChain_)) {
+        std::cout << "\033[32m" << "numbers is sorted: Yes" << "\033[0m" << std::endl;
+    } else {
+        std::cout << "\033[31m" << "numbers is sorted: No" << "\033[0m" << std::endl;
+    }
+
+    arr.sort();
+
+    
+    if (mainChain_ == arr) {
+        std::cout << "\033[32m" << "Lists are equal." << "\033[0m" << std::endl;
+    } else {
+        std::cout << "\033[31m" << "Lists are not equal." << "\033[0m" << std::endl;
+    }
+
+
+}
+
---
+++ cpp-module09/vector/PmergeMe.hpp
@@ -0,0 +1,171 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <iostream>
+# include <string>
+# include <list>
+# include <list>
+# include <exception>
+# include <algorithm>
+# include <sstream>
+# include <stdexcept>
+
+
+class PmergeMe
+{
+    typedef std::list<int>::const_iterator ConstIntListIter;
+    typedef std::list<int>::iterator IntListIter;
+
+    typedef std::list<std::list<int>::const_iterator> ConstIteratorList;
+    typedef std::list<std::list<int>::const_iterator>::const_iterator ConstIteratorListIterator;
+
+    struct UnpairedData {
+        size_t length;
+        int value;
+        int position;
+    };
+
+    private:
+        std::list<int>          mainChain_;
+    
+    private:
+        void    initContainer(int size, int numbers[]);
+
+    public:
+        PmergeMe();
+        PmergeMe(const PmergeMe & r);
+        ~PmergeMe();
+        PmergeMe & operator= (const PmergeMe &other);
+ 
+    void FordJohnsonAlgorithm(int size, int numbers[]);
+    void mergeInsertionSort(std::list<int> & mainChain,int pairSize);
+
+
+    //pair
+    bool    pairCompare( std::list<int> & lst, int pairSize, int start );
+    void    pairSwap( std::list<int> & lst, int pairSize, int start );
+    bool    hasPair(std::list<int> & lst, int pairSize, size_t start );
+    void    makePair( std::list<int> & lst, std::list<int> & subChain, int pairSize );
+
+    //separate
+    void    separateMainChainAndSubChain(std::list<int> & mainChain, std::list<int> & subChain, int pairSize);
+    
+    // first insert
+    void    insertAtTheStart(std::list<int> & mainChain, std::list<int> & subChain, int pairSize);
+
+    // insert
+    void                        insertionFromSubIntoMain(std::list<int> & mainChain, std::list<int> & subChain, int pairSize, UnpairedData& unpair);
+    int                         jacobsthalNumber(int n);
+    int                         binarySearch(std::list<int> & mainChain, int key,int pairSize, UnpairedData& unpair, int endpoint);
+    bool                        isKey(std::list<int> & lst, int index, int key);
+    int                         lower_bound(std::list<int> & lst, int key, int right);
+    void                        insertSubChain(std::list<int> & mainChain, int insertPosition, ConstIntListIter begin, ConstIntListIter end);
+
+
+
+
+
+
+    template <typename T>
+    typename std::list<T>::iterator advanceTo(std::list<T>& lst, size_t index) {
+        if (index >= lst.size()) { return lst.end(); }
+
+        typename std::list<T>::iterator it = lst.begin();
+        std::advance(it, index);
+        return it;
+    }
+    template <typename T>
+    typename std::list<T>::const_iterator advanceIt(std::list<T>& lst, typename std::list<T>::const_iterator itr, size_t index) {
+
+        for (size_t i=0; i< index ; ++i) {
+            ++itr;
+            if(itr == lst.end()) return lst.end();
+        }
+        return itr;
+    }
+
+    template <typename T>
+    T getElementAtIndex(const std::list<T>& lst, size_t index) {
+        typename std::list<T>::const_iterator it = lst.begin();
+        for (size_t i = 0; i < index; ++i) {
+            if (it == lst.end()) {
+                throw std::out_of_range("Index out of bounds");
+            }
+            ++it;
+        }
+
+        if (it == lst.end()) {
+            throw std::out_of_range("Index out of bounds");
+        }
+
+        return *it;
+    }
+
+    //debug
+    public:
+        template <typename T, typename Iter>
+        static void print(T t) {
+            for(Iter it=t.begin(); it != t.end(); ++it) {
+                std::cout << *it << " ";
+            }
+            std::cout << std::endl;
+        }
+                template <typename T>
+        void printDebug(const T& mainChain, const T& subChain, int pairSize, const char* message) {
+            typedef typename T::const_iterator Iterator;
+            if(strncmp(message,"B",1) == 0)
+                std::cout << std::endl << "========================================================================================" << std::endl;
+            std::cout << std::endl << message << " pairsize: " << pairSize << "   ( " << (pairSize*2) << " )" << std::endl;
+            if(!mainChain.empty()) {
+                std::cout << "mainChain" << std::endl;
+                for(Iterator it = mainChain.begin(); it != mainChain.end(); ++it) {
+                    int index = std::distance(mainChain.begin(), it);
+
+                    if(index % (pairSize * 2) == 0) { std::cout << "[" ; }
+                    std::cout << *it;
+                    if(index % (pairSize * 2) == pairSize * 2 - 1 || it == std::prev(mainChain.end())) { std::cout << "]" ; }
+                    if ( (index % (pairSize * 2) == pairSize - 1) && (it != std::prev(mainChain.end())) ) { std::cout << " | "; }
+                    else { std::cout << "  "; }
+                }
+                std::cout << std::endl;
+            }
+
+            if(!subChain.empty()) {
+                std::cout << "subChain" << std::endl;
+                for(Iterator it = subChain.begin(); it != subChain.end(); ++it) {
+                    int index = std::distance(subChain.begin(), it);
+                    if( (index % (pairSize) == 0) && (it != subChain.begin()) ) { 
+                        std::cout << " | " << *it;
+                    } else {
+                        std::cout << " " << *it << " ";
+                    }
+                }
+                std::cout << std::endl;
+            }
+            if(strncmp(message,"A",1) == 0)
+                std::cout << std::endl << "========================================================================================" << std::endl;
+        }
+
+        bool isSorted(const std::list<int>& lst) {
+            if (lst.empty()) {
+                return true;
+            }
+
+            std::list<int>::const_iterator it = lst.begin();
+            std::list<int>::const_iterator next_it = it;
+            ++next_it;
+
+            while (next_it != lst.end()) {
+                if (*it > *next_it) {
+                    return false;
+                }
+                ++it;
+                ++next_it;
+            }
+
+            return true;
+        }
+
+};
+
+#endif
