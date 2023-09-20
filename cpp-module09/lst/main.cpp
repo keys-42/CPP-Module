16c01c35b3b3d9e01b6e2703fcd5a32d7579df51
libknt <101494707+libknt@users.noreply.github.com>
2023-09-20 17:56:58 +0900

    refactor step2

--- cpp-module09/lst/main.cpp
+++ cpp-module09/lst/main.cpp
@@ -11,24 +11,6 @@ int main( int argc, char **argv )
   int n = argc - 1;
   int* arr = new int[n];
 
-//   int n = 42;
-//   int* arr = new int[n];
-// int arr1[] = {
-//        90, 32, 61, 58, 22, 92, 40, 55, 36, 11, 
-//        87, 30, 4,  59, 23, 39, 5,  13, 82, 18, 
-//        26, 84, 21, 8,  44, 1,  46, 43, 14, 29, 
-//        62, 79, 71, 99, 91, 50, 51, 88, 35, 54, 
-//        73, 56, 53,
-//       };
-//   int arr1[] = {
-//     53, 95, 35, 36, 24, 20, 97, 71, 78, 48, 
-//     50, 25, 28, 56, 74, 91, 43, 23, 90, 14, 
-//     31, 89, 38, 98, 93, 57, 33, 99, 77, 61, 
-//     63, 52, 87, 80, 19, 2, 72, 21, 94, 65, 
-//     60, 22};
-  // for(int i = 1; i <= n; ++i) {
-  //     arr[i-1] = arr1[i-1];
-  // }  
   for(int i = 1; i < argc; ++i) {
       arr[i-1] = std::atoi(argv[i]);
   }  
@@ -43,19 +25,7 @@ int main( int argc, char **argv )
   PmergeMe *p = new PmergeMe();
   p->FordJohnsonAlgorithm(n,arr);
   
-  // std::cout << "std::sort  " << std::endl;
-  // std::sort(arr, arr + n);
-  // for(int i = 0; i < n; ++i) {
-  //     std::cout << arr[i] << " ";
-  // }
-  // std::cout << std::endl;
-
   delete p;
   delete[] arr;
   return 0;
 }
-
-// Input
-// 57 18 36 84 74 87 20 78 46 6 72 17 24 64 7 14 45 92 23 33 54 88 66 48 93 82 9 11 83 49 53 3 37 59 21 27 4 69 80 35 43 28 60 16 56 100 77 79 91 73 32 34 8 86 52 22 90 5 67 31 68 81 29
-// FordJohnson
-// Index out of bounds
