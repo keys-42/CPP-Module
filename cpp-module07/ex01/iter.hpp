03079d30824d3a8a634eff317fd89938fc2a2e40
libknt <101494707+key-door@users.noreply.github.com>
2023-09-23 19:19:19 +0900

    fix

--- cpp-module07/ex01/iter.hpp
+++ cpp-module07/ex01/iter.hpp
@@ -7,7 +7,7 @@
 #include <iomanip>
 
 template <typename T,typename L>
-void iter(T *ptr, L len, void (*f)(T &))
+void iter(T *ptr, L len, void (*f)(const T &))
 {
     for(L i = 0; i < len; i++) {
         f(ptr[i]);
@@ -15,9 +15,17 @@ void iter(T *ptr, L len, void (*f)(T &))
 }
 
 template <typename T>
-void func(T &p)
+void func(const T &p)
 {
     std::cout << std::setw(10) << p << std::endl;
+}
+
+
+template <typename T>
+void multiplyByPi(const T &value) {
+    double result = value * M_PI;
+    std::cout << std::fixed << std::setprecision(3);
+    std::cout << std::setw(10) << result << std::endl;
 }
 
 #endif
--- cpp-module07/ex01/main.cpp
+++ cpp-module07/ex01/main.cpp
@@ -1,15 +1,15 @@
 #include "iter.hpp"
 
+void printLength(const std::string &s)
+{
+    std::cout << std::setw(10) << s.length() << std::endl;
+}
+
 void printLength(std::string &s)
 {
     std::cout << std::setw(10) << s.length() << std::endl;
 }
 
-void multiplyByPi(int &value) {
-    double result = value * M_PI;
-    std::cout << std::fixed << std::setprecision(3);
-    std::cout << std::setw(10) << result << std::endl;
-}
 
 int main( void ) 
 {
@@ -26,13 +26,27 @@ int main( void )
         "9",
     };
 
+    const std::string constArr[] = {
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+    };
+
     int size = sizeof(arr) / sizeof(arr[0]);
 
     std::cout << "{" << std::endl;
-    iter(arr,size,&func);
+    iter(constArr,size,&func);
     std::cout << "}" << std::endl;
+    std::cout <<"+++++++++++++++++" <<std::endl;
     std::cout << "{" << std::endl;
-    iter(arr,size,&printLength);
+    iter(arr,size,&func);
     std::cout << "}" << std::endl;
 
     int i[] = {
@@ -49,8 +63,8 @@ int main( void )
     };
 
     std::cout << "}" << std::endl;
-    std::cout << "{" << std::endl;
     iter(i,10,&multiplyByPi);
+    std::cout << "{" << std::endl;
 
 
     return 0;
