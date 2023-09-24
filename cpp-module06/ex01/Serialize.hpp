e898996c8dbab1e1da355e036b076d2e51409df1
libknt <101494707+libknt@users.noreply.github.com>
2023-09-24 15:54:02 +0900

    fix

--- cpp-module06/ex01/Serialize.hpp
+++ cpp-module06/ex01/Serialize.hpp
@@ -3,14 +3,17 @@
 
 #include "Data.hpp"
 
-// #if defined(_WIN64) || defined(__x86_64__) || defined(__ppc64__)
-// typedef unsigned long long uintptr_t;
-// #else
-// typedef unsigned long uintptr_t;
-// #endif
+#if defined(_WIN64) || defined(__x86_64__) || defined(__ppc64__)
+typedef unsigned long long uintptr_t;
+#else
+typedef unsigned long uintptr_t;
+#endif
 
 class Serialize
 {
+    private:
+        Serialize(){};
+
     public:
         static uintptr_t serialize(Data* ptr);
         static Data* deserialize(uintptr_t raw);
