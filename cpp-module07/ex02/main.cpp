f709a7f19a74e3de512f34d2a49d0ff245637e66
libknt <101494707+key-door@users.noreply.github.com>
2023-09-25 22:26:25 +0900

    fix

--- cpp-module07/ex02/main.cpp
+++ cpp-module07/ex02/main.cpp
@@ -7,122 +7,147 @@
 int main()
 {
     {
-        std::cout << GREEN;
-     	std::cout << std::endl << "--------empty array-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
-
-        Array<int> a;
-        std::cout << "operator<< a: [" << a << "]" << std::endl;
-        std::cout << "try operator[] index" << std::endl;
         try {
-            std::cout << a[0] << std::endl;
-        } catch (Array<int>::OutOfBoundsException &e) {
-            std::cout << e.what() <<std::endl;
+            std::cout << GREEN;
+            std::cout << std::endl << "--------empty array-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+
+            Array<int> a;
+            std::cout << "operator<< a: [" << a << "]" << std::endl;
+            std::cout << "try operator[] index" << std::endl;
+            try {
+                std::cout << a[0] << std::endl;
+            } catch (Array<int>::OutOfBoundsException &e) {
+                std::cout << e.what() <<std::endl;
+            }
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
+            std::exit(1);
         }
     }
     {
-        std::cout << GREEN;
-     	std::cout << std::endl << "--------char array-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
-
-        Array<char > a(5);
-        std::cout << "operator<< a: " << a << std::endl;
-        for (unsigned int i = 0; i < a.size(); i++)
-            a[i] = i + 66;
-        std::cout << "operator<< a: " << a << std::endl;
-        std::cout << "try operator[] index" << std::endl;
         try {
-            for(unsigned int i=0; i < 10; ++i) {
-                std::cout << a[i] << std::endl;
+            std::cout << GREEN;
+            std::cout << std::endl << "--------char array-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+
+            Array<char > a(5);
+            std::cout << "operator<< a: " << a << std::endl;
+            for (unsigned int i = 0; i < a.size(); i++)
+                a[i] = i + 66;
+            std::cout << "operator<< a: " << a << std::endl;
+            std::cout << "try operator[] index" << std::endl;
+            try {
+                for(unsigned int i=0; i < 10; ++i) {
+                    std::cout << a[i] << std::endl;
+                }
+            } catch (std::exception &e) {
+                std::cout << e.what() <<std::endl;
             }
         } catch (std::exception &e) {
-            std::cout << e.what() <<std::endl;
+            std::cerr << e.what() << std::endl;
+            std::exit(1);
         }
     }
     {
-        std::cout << GREEN;
-     	std::cout << std::endl << "--------std::string array-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
-
-        Array<std::string> a(5);
-        std::cout << "operator<< a: " << a << std::endl;
-        for (unsigned int i = 0; i < a.size(); i++)
-            a[i] = i + 66;
-        std::cout << "operator<< a: " << a << std::endl;
-        std::cout << "try operator[] index" << std::endl;
         try {
-            for(unsigned int i=0; i < 10; ++i) {
-                std::cout << a[i] << std::endl;
+            std::cout << GREEN;
+            std::cout << std::endl << "--------std::string array-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+
+            Array<std::string> a(5);
+            std::cout << "operator<< a: " << a << std::endl;
+            for (unsigned int i = 0; i < a.size(); i++)
+                a[i] = i + 66;
+            std::cout << "operator<< a: " << a << std::endl;
+            std::cout << "try operator[] index" << std::endl;
+            try {
+                for(unsigned int i=0; i < 10; ++i) {
+                    std::cout << a[i] << std::endl;
+                }
+            } catch (std::exception &e) {
+                std::cout << e.what() <<std::endl;
             }
         } catch (std::exception &e) {
-            std::cout << e.what() <<std::endl;
+            std::cerr << e.what() << std::endl;
+            std::exit(1);
         }
     }
     {
-        std::cout << GREEN;
-     	std::cout << std::endl << "--------double array-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
-
-        Array<double> a(5);
-        std::cout << "operator<< a: " << a << std::endl;
-        for (unsigned int i = 0; i < a.size(); i++)
-            a[i] = i + M_PI;
-        std::cout << "operator<< a: " << a << std::endl;
-        std::cout << "try operator[] index" << std::endl;
         try {
-            for(unsigned int i=0; i < 10; ++i) {
-                std::cout << a[i] << std::endl;
+            std::cout << GREEN;
+            std::cout << std::endl << "--------double array-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+
+            Array<double> a(5);
+            std::cout << "operator<< a: " << a << std::endl;
+            for (unsigned int i = 0; i < a.size(); i++)
+                a[i] = i + M_PI;
+            std::cout << "operator<< a: " << a << std::endl;
+            std::cout << "try operator[] index" << std::endl;
+            try {
+                for(unsigned int i=0; i < 10; ++i) {
+                    std::cout << a[i] << std::endl;
+                }
+            } catch (std::exception &e) {
+                std::cout << e.what() <<std::endl;
             }
         } catch (std::exception &e) {
-            std::cout << e.what() <<std::endl;
+            std::cerr << e.what() << std::endl;
+            std::exit(1);
         }
     }
     std::cout << std::endl;
     {
-        std::cout << GREEN;
-     	std::cout << std::endl << "--------simple test-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
-        unsigned int n = (unsigned int)N;
-        Array<int> a(n);
-        Array<int> b(n);
-        Array<int> c(a);
-
-        b = a;
-        std::cout << "a: " << a << std::endl;
-        std::cout << "b: " << b << std::endl;
-        std::cout << "c: " << c << std::endl;
-        
-        
-        for (unsigned int i = 0; i < a.size(); i++)
-            a[i] = i ;
-        for (unsigned int i = 0; i < a.size(); i++)
-            b[i] = i * M_PI;
-        for (unsigned int i = 0; i < a.size(); i++)
-            c[i] = a[i] * b[i];
-        
-        std::cout << GREEN;
-     	std::cout << std::endl << "a = 0, 1, 2, 3, 4  " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
-        std::cout << "a: " << a << std::endl;
-
-        std::cout << GREEN;
-     	std::cout << std::endl << "b = a*π  " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
-        std::cout << "b: " << b << std::endl;
-
-        std::cout << GREEN;
-     	std::cout << std::endl << "c = b * a  " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
-        std::cout << "c: " << c << std::endl;
-        std::cout << GREEN;
-     	std::cout << std::endl << "try operator[] index " << __FILE__ << " in " << __LINE__ << std::endl;   
-        std::cout << RESET;
         try {
-            for(unsigned int i=0; i < a.size() + 10; ++i) {
-                std::cout << a[i] << std::endl;
+            std::cout << GREEN;
+            std::cout << std::endl << "--------simple test-------------------- " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+            unsigned int n = (unsigned int)N;
+            Array<int> a(n);
+            Array<int> b(n);
+            Array<int> c(a);
+
+            b = a;
+            std::cout << "a: " << a << std::endl;
+            std::cout << "b: " << b << std::endl;
+            std::cout << "c: " << c << std::endl;
+            
+            
+            for (unsigned int i = 0; i < a.size(); i++)
+                a[i] = i ;
+            for (unsigned int i = 0; i < a.size(); i++)
+                b[i] = i * M_PI;
+            for (unsigned int i = 0; i < a.size(); i++)
+                c[i] = a[i] * b[i];
+            
+            std::cout << GREEN;
+            std::cout << std::endl << "a = 0, 1, 2, 3, 4  " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+            std::cout << "a: " << a << std::endl;
+
+            std::cout << GREEN;
+            std::cout << std::endl << "b = a*π  " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+            std::cout << "b: " << b << std::endl;
+
+            std::cout << GREEN;
+            std::cout << std::endl << "c = b * a  " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+            std::cout << "c: " << c << std::endl;
+            std::cout << GREEN;
+            std::cout << std::endl << "try operator[] index " << __FILE__ << " in " << __LINE__ << std::endl;   
+            std::cout << RESET;
+            try {
+                for(unsigned int i=0; i < a.size() + 10; ++i) {
+                    std::cout << a[i] << std::endl;
+                }
+            } catch (std::exception &e) {
+                std::cout << e.what() <<std::endl;
             }
         } catch (std::exception &e) {
-            std::cout << e.what() <<std::endl;
+            std::cerr << e.what() << std::endl;
+            std::exit(1);
         }
     }
     return 0;
