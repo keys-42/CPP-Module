1ed2f98f6bea9e8c4deb6ec192bb0c0dce571c75
libknt <101494707+key-door@users.noreply.github.com>
2023-09-25 22:23:33 +0900

    fix

--- cpp-module07/ex01/iter.hpp
+++ cpp-module07/ex01/iter.hpp
@@ -7,24 +7,41 @@
 #include <iomanip>
 
 template <typename T,typename L>
-void iter(T *ptr, L len, void (*f)(const T &))
+void iter(T *ptr, L len, void (*f)(T &))
 {
+    if ( !ptr ) throw std::invalid_argument("Received null as argument"); 
+    for(L i = 0; i < len; i++) {
+        f(ptr[i]);
+    }
+}
+
+template <typename T,typename L>
+void iter(T const *ptr, L len, void (*f)(const T &))
+{
+    if ( !ptr ) throw std::invalid_argument("Received null as argument"); 
     for(L i = 0; i < len; i++) {
         f(ptr[i]);
     }
 }
 
 template <typename T>
-void func(const T &p)
-{
-    std::cout << std::setw(10) << p << std::endl;
+void multiplyByPi(T &value) {
+    value = static_cast<int>(M_PI) * value;
 }
 
 template <typename T>
-void multiplyByPi(const T &value) {
-    double result = value * M_PI;
-    std::cout << std::fixed << std::setprecision(3);
-    std::cout << std::setw(10) << result << std::endl;
+void multiplyByPi(T const &value) {
+    std::cout << static_cast<int>(M_PI) * value << " ";
+}
+
+template <typename T,typename L>
+void print(T const *ptr, L len)
+{
+    if ( !ptr ) throw std::invalid_argument("Received null as argument"); 
+    for(L i = 0; i < len; i++) {
+        std::cout << ptr[i] << " ";
+    }
+    std::cout << std::endl;
 }
 
 #endif
